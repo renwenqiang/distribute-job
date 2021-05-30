892c289083995758d8099151333dc11209a99035
renwq <renwenqiang@tripowers.net>
2021-05-30 22:18:53 +0800

    gj30

---
+++ distribute-job-admin/src/main/java/com/distribute/job/admin/core/route/strategy/ExecutorRouteFirst.java
@@ -0,0 +1,19 @@
+package com.distribute.job.admin.core.route.strategy;
+
+import com.distribute.job.admin.core.route.ExecutorRouter;
+import com.distribute.job.core.biz.model.ReturnT;
+import com.distribute.job.core.biz.model.TriggerParam;
+
+import java.util.List;
+
+/**
+ * Created by xuxueli on 17/3/10.
+ */
+public class ExecutorRouteFirst extends ExecutorRouter {
+
+    @Override
+    public ReturnT<String> route(TriggerParam triggerParam, List<String> addressList){
+        return new ReturnT<String>(addressList.get(0));
+    }
+
+}
